448565a301cb7b02d3ac8e3d4a3762d3aaf2dbc7
0p3n <54430946+0pen3r@users.noreply.github.com>
2023-05-16 16:20:15 +0900

    Update 2022_06_03_GetPassNParsing.py
    .. #print(blabla)해서 30분날림

--- 2022_06_03_GetPassNParsing.py
+++ 2022_06_03_GetPassNParsing.py
@@ -13,24 +13,33 @@ print('\n\n[passwd get start]\n')
 
 #파싱 전의 passwords 담을 리스트 선언
 pw_list_aix =[]
+pw_list_aix_smd =[]
 pw_list_linux =[]
 
 #파일 몇개 돌렸는지 카운딩 상수
 c_aix = 0
 c_linux = 0
 
+##aix pass 형식
+##{smd5}, {ssha1}, {ssha256}, {ssha512}
+
+##linux /etc/shadow pass 
+## $1:md5(취약), $5:sha256, $6:sha512, $y:yscript(john으로 가능)
+## $2a:b-crypt, $md5:sun md5
+
+##salt 예시
+# {ssha512}06$ㄴㅁㄹㄴㅁㄹㅇㄴ$asfafdsfsdfdsfdsfsfdfdsfsd
+# $6$소금값$o2o2o2o2o2o2o2oo2o2o2o2o2
 
 def pass_parse_AIX(pw_list_aix, pw_list_linux):
-
 	for file_name in file_list:
-
 		with open(file_path+file_name, 'rb') as f:
 			lines = f.readlines()
 			#global pw_list_aix 빼고 pass_parse_AIX(pw_list_aix)로 인자 투입
 
 			for line in lines:
-
 				#ssha 형식은 AIX니까 AIX 분류
+				#if b'ssha' in line:로해도됨 
 				if 'ssha'.encode() in line:
 					#파싱1 XX = {ssha512}
 					aaa = str(line).split()
@@ -39,16 +48,26 @@ def pass_parse_AIX(pw_list_aix, pw_list_linux):
 					#리스트에 append
 					pw_list_aix.append(bbb[0])
 
+				elif 'smd5'.encode() in line:
+					#파싱1 XX = {ssha512}
+					aaa = str(line).split()
+					#파싱2 ..\\n
+					bbb = aaa[2].split('\\')
+					#리스트에 append
+					pw_list_aix_smd.append(bbb[0])
+				
 				elif ':$6$'.encode() in line:
 					#파싱1 : 으로 구분
 					aaa = str(line).split(':')
 					#리스트에 append
 					pw_list_linux.append(aaa[1])
 
-
+					
+#출력부분
 pass_parse_AIX(pw_list_aix,pw_list_linux)
 print('\n=== AIX Password List ===\n')
-#print(pw_list_aix)
+print(pw_list_aix)
+print(pw_list_aix_smd)
 print('\n=== Linux Password List ===\n')
 print(pw_list_linux)
 
